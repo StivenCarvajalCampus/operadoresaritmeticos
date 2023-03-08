66af092ce4f6c0628d068d8a02b25d92b183a04c
StivenCarvajalCampus <Stivencarvajal3211@gmail.com>
2023-03-08 13:16:30 -0500

    feat(Resta): :sparkles: Multiplicacion
    se utiliza para multiplicar dos valores.

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-//Resta (-): se utiliza para restar un valor de otro.
-let x = 35;
-let y = 18;
-let z = x - y;
-console.log(z); // Output: 17
+//Multiplicación (*): se utiliza para multiplicar dos valores.
+let x = 25;
+let y = 15;
+let z = x * y;
+console.log(z); // Output: 375
